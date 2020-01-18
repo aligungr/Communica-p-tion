761a5b81a725de47ce40b61e0ca3f1423f194c1b
ali-gungor <aligng1620@gmail.com>
2020-01-18 20:12:18 +0300

    recognition listener proxy impl.

---
+++ Assets/Scripts/PrimeTech/SpeechRecognizer/SpeechRecognizerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrimeTech.SpeechRecognizer {
+
+    public class SpeechRecognizerController : MonoBehaviour {
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
