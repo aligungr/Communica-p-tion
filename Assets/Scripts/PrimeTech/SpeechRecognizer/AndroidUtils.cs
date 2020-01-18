5d00bac9c278c53a59dbef829f5076d2dd170317
ali-gungor <aligng1620@gmail.com>
2020-01-18 20:57:58 +0300

    Android Speech Recognizer impl

---
+++ Assets/Scripts/PrimeTech/SpeechRecognizer/AndroidUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PrimeTech.SpeechRecognizer {
+
+    public static class AndroidUtils {
+
+        public static AndroidJavaObject GetActivityContext() {
+            var jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            var jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+            return jo;
+        }
+    }
+}
