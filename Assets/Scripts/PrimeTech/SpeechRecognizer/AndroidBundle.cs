f647d98ef5247e6a5dc8990e4f2c369530c71ae6
ali-gungor <aligng1620@gmail.com>
2020-01-18 20:12:18 +0300

    recognition listener proxy impl.

---
+++ Assets/Scripts/PrimeTech/SpeechRecognizer/AndroidBundle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrimeTech.SpeechRecognizer {
+
+    public class AndroidBundle {
+        private readonly AndroidJavaObject obj;
+
+        public AndroidBundle(AndroidJavaObject bundleObject) {
+            this.obj = bundleObject;
+        }
+
+        public List<string> GetStringList(string key) {
+            var result = new List<string>();
+
+            var arrayList = obj.Call<AndroidJavaObject>("getStringArrayList", key);
+            int size = arrayList.Call<int>("size");
+            for (int i = 0; i < size; i++) {
+                string str = arrayList.Call<string>("get", i);
+                result.Add(str);
+            }
+
+            return result;
+        }
+
+        public float[] GetFloatArray(string key) {
+            return obj.Call<float[]>("getFloatArray", key);
+        }
+    }
+}
+
