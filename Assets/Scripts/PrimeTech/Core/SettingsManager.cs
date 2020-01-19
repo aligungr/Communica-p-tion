6850d5e7dedca5f1f14ccd02954c81eee162cafc
beyzaaltuntas <30345525+beyzaaltuntas@users.noreply.github.com>
2020-01-19 23:55:59 +0300

    .gitignore fix

---
+++ Assets/Scripts/PrimeTech/Core/SettingsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace PrimeTech.Core
+{
+    public class SettingsManager : MonoBehaviour
+    {
+        public static void SetMode(int mode)
+        {
+            PlayerPrefs.SetInt("mode", mode);
+        }
+
+        public static int GetMode()
+        {
+            return PlayerPrefs.GetInt("mode");
+        }
+
+        public static void SetFaceDetection(int faceDetection)
+        {
+            PlayerPrefs.SetInt("faceDetection", faceDetection);
+        }
+
+        public static int GetFaceDetection()
+        {
+            return PlayerPrefs.GetInt("faceDetection");
+        }
+
+        public static void SetTranslateLanguage(int translateLanguage)
+        {
+            PlayerPrefs.SetInt("translateLanguage", translateLanguage);
+        }
+
+        public static int GetTranslateLanguage()
+        {
+            return PlayerPrefs.GetInt("translateLanguage");
+        }
+
+    }
+
+}
