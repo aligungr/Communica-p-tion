95116152d5c9d531330ff72c33e3638b87c3ae22
ali-gungor <aligng1620@gmail.com>
2020-01-19 16:42:04 +0300

    ConsoleController and Statup structure introduced

---
+++ Assets/Scripts/PrimeTech/Core/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrimeTech.Core {
+    public class Startup : MonoBehaviour {
+        public void Awake() {
+            Application.logMessageReceived += (string condition, string stackTrace, LogType type) => {
+                var console = FindObjectOfType<ConsoleController>();
+                if (console != null) {
+                    console.AppendLog(condition, type);
+                }
+            };
+        }
+
+        private void Start() {
+            // Write your startup codes here
+        }
+    }
+}
+
