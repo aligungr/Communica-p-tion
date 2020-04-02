44259fc15a0f5717ef6b5bd0375112be289f6a23
dilaradogan <dilaradogan097@gmail.com>
2020-04-02 19:02:06 +0300

    Pin and Welcome scenes and their controlling scripts have been added.

---
+++ Assets/Scripts/PrimeTech/Core/PinScreenController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static HttpRequest;
+
+namespace PrimeTech.Core
+{
+    public class PinScreenController : MonoBehaviour
+    {
+        public GameObject pinArea;
+        public Button buttonPin;
+        public GameObject pinScreen;
+        private WelcomeController welcome;
+        private string pin;
+        public int userId;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+            welcome = GameObject.FindObjectOfType<WelcomeController>();
+            buttonPin.onClick.AddListener(validatePin);
+        }
+
+        private void validatePin()
+        {
+            pin = pinArea.GetComponent<InputField>().text;
+            Debug.Log(pin);
+            string url = "http://localhost:64021/connectWithHololens" + pin;
+            // string a = null;
+            byte[] array = null;
+            HttpResponseHandler myHandler1 = (int statusCode, string responseText, byte[] responseData) =>
+             {
+                 if (statusCode == 200)
+                 {
+                     if (responseData != null)
+                     {
+                         userId = BitConverter.ToInt32(responseData, 0);
+                         welcome.updateId(userId);
+                         SceneManager.LoadScene("OptionsUI");
+                     }
+                     else
+                     {
+                         SceneManager.LoadScene("Welcome");
+                     }
+                 }
+             };
+            HttpRequest.Send(this, UnityWebRequest.kHttpVerbPOST, url, null, array, myHandler1);
+
+        }
+    }
+}
---
+++ Assets/Scripts/PrimeTech/Core/WelcomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static HttpRequest;
+namespace PrimeTech.Core
+{
+    public class WelcomeController : MonoBehaviour
+    {
+        public Button buttonWithHololens;
+        public Button buttonWithoutHololens;
+        public GameObject welcomeScreen;
+        public Text id;
+        public int userId;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            buttonWithHololens.onClick.AddListener(continueWithHololens);
+            buttonWithoutHololens.onClick.AddListener(continueWithoutHololens);
+        }
+        private void continueWithHololens()
+        {
+            SceneManager.LoadScene("Pin");
+        }
+        private void continueWithoutHololens()
+        {
+            string url = "http://localhost:64021/connectWithoutHololens";
+            //  string a = null;
+            byte[] array = null;
+            HttpResponseHandler myHandler1 = (int statusCode, string responseText, byte[] responseData) =>
+            {
+                if (statusCode == 200)
+                {
+                    if (responseData != null)
+                    {
+                        userId = BitConverter.ToInt32(responseData, 0);
+                        id.GetComponent<Text>().text = userId.ToString();
+                        SceneManager.LoadScene("OptionsUI");
+                    }
+                }
+               /* else
+                {
+                    Debug.Log(statusCode);
+                    Debug.Log(responseText);
+                }*/
+            };
+            HttpRequest.Send(this, UnityWebRequest.kHttpVerbPOST, url, null, array, myHandler1);
+        }
+        public void updateId(int newId)
+        {
+            if(newId != 0)
+            {
+                id.GetComponent<Text>().text = newId.ToString();
+            }
+            
+        }
+    }
+}
